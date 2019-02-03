b91f18ee3060f75ca71250eaef83343e9c95fbce
abalarin <abalarin@outlook.com>
2019-02-03 15:17:56 -0500

    bootstraped

--- app.py
+++ app.py
@@ -2,16 +2,23 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import Navbar, View
+from data import Products
 
+# Creating a navbar object
 topbar = Navbar('MyBar',
     View('Home', 'index'),
     View('Profile', 'profile'),
 )
 
+Products = Products()
+
 def create_app():
+
+  #Instantiate Application
   app = Flask(__name__)
   Bootstrap(app)
 
+  # NAV Bar config
   nav = Nav()
   nav.register_element('top', topbar)
 
@@ -19,14 +26,13 @@ def create_app():
   # later on, initialize your app:
   nav.init_app(app)
 
+  # Routes
   @app.route('/')
   def index():
-      return render_template("index.html")
+      return render_template("index.html", products = Products)
 
   @app.route('/profile')
   def profile():
       return render_template("profile.html")
 
   return app
-
-# do something with app...
