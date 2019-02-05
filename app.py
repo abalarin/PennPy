f259d2c55df4282a981b744a74a32f532338f83e
abalarin <abalarin@outlook.com>
2019-02-05 13:06:56 -0500

    switchf rom flask-nav to html

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from flask import Flask, render_template, flash, request, redirect, url_for, session, logging
 
-# NAv Bar Requirments
-from flask_nav import Nav
-from flask_nav.elements import *
-
 # SQL
 from flask_mysqldb import MySQL
 
@@ -16,29 +12,11 @@ from passlib.hash import sha256_crypt
 from data import Products
 Products = Products()
 
-# Creating a navbar object
-topbar = Navbar('eStitch',
-    View('Home', 'index'),
-    View('Profile', 'profile'),
-
-    View('Login', 'login'),
-    View('Register', 'register'),
-    View('Logout', 'logout')
-)
-
 def create_app():
 
   #Instantiate Application
   app = Flask(__name__)
   Bootstrap(app)
-
-  # NAV Bar config
-  nav = Nav()
-  nav.register_element('top', topbar)
-
-  # [...]
-  # later on, initialize your app:
-  nav.init_app(app)
 
   # #MYSQL init
   app.config['MYSQL_HOST']='localhost'
@@ -55,7 +33,6 @@ def create_app():
   # ----- Routes -----
   @app.route('/')
   def index():
-
       return render_template("index.html", products = Products)
 
   @app.route('/profile')
