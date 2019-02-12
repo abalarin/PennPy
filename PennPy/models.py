f6b378dd4381b17e1bee898c11d9976d17b44eb6
abalarin <abalarin@outlook.com>
2019-02-12 13:58:42 -0500

    sql modeling

---
+++ PennPy/models.py
@@ -0,0 +1,11 @@
+from PennPy import db
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
