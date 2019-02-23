e8af0adf12500c57a241b719ef2aac6e60529563
abalarin <abalarin@outlook.com>
2019-02-23 11:06:20 -0500

    refactor

--- PennPy/config.py
+++ PennPy/config.py
@@ -5,7 +5,7 @@ class Config:
 
     DB_USER = os.environ.get('DB_USER')
     DB_PASS = os.environ.get('DB_PASS')
-    SECRET_KEY = "developmenttest"  # os.urandom(12)
+    SECRET_KEY = os.urandom(12)
 
     # MYSQL init
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://' + DB_USER + ':' + DB_PASS + '@localhost:3306/PennPy'
