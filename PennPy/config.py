c909c7cab9b946e3967f1d74e6725357370eb3e3
abalarin <abalarin@outlook.com>
2019-02-23 23:02:24 -0500

    crud addresses

--- PennPy/config.py
+++ PennPy/config.py
@@ -5,12 +5,12 @@ class Config:
 
     DB_USER = os.environ.get('DB_USER')
     DB_PASS = os.environ.get('DB_PASS')
-    SECRET_KEY = "dev"  # os.urandom(12)
+    SECRET_KEY = os.urandom(12)
 
     # Postgres entry
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://' + DB_USER + ':' + DB_PASS + '@45.33.79.194:5432/PennPy'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://' + DB_USER + ':' + DB_PASS + '@45.33.79.194:5432/PennPy'
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql://abalarin:lindoe!@localhost:5432/PennPy'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://abalarin:lindoe!@localhost:5432/PennPy'
 
     # To suppress FSADeprecationWarning
     SQLALCHEMY_TRACK_MODIFICATIONS = False
