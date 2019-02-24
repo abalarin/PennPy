ada1b3d56a655948aa51ee382e23bd0737443514
abalarin <abalarin@outlook.com>
2019-02-24 18:47:24 -0500

    setup

---
+++ PennPy/config.py
@@ -0,0 +1,20 @@
+import os
+
+
+class Config:
+
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+    SECRET_KEY = os.urandom(12)
+
+    # Connection to Postgres server
+    SQLALCHEMY_DATABASE_URI = 'postgresql://' + DB_USER + ':' + DB_PASS + '@45.33.79.194:5432/PennPy'
+
+    # Connection to local postgres db
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://abalarin:lindoe!@localhost:5432/PennPy'
+
+    # To suppress FSADeprecationWarning
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Gets pwd and declares it is the root dir for the App
+    APP_ROOT = os.path.dirname(os.path.abspath(__file__))
